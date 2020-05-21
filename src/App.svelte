2df8c05cb83bd996fe3b14d46da6fbb64bde9521
Steve Bargelt <steve@bargelt.com>
2020-05-21 07:58:33 -0700

    Initial commit

---
+++ src/App.svelte
@@ -0,0 +1,14 @@
+<script>
+  import Tailwindcss from "./Tailwindcss.svelte";
+
+  export let name;
+</script>
+
+<style>
+  h1 {
+    @apply bg-black text-white;
+  }
+</style>
+
+<Tailwindcss />
+<h1>Hello {name}!</h1>
